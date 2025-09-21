fa2223bca5eee1cfa41d297b194a3917436c4312
tkin91e55 <tkingkwun@gmail.com>
2025-09-21 12:37:04 +0800

    [Django] add cors exemption

--- django/wordle/wordle/settings.py
+++ django/wordle/wordle/settings.py
@@ -25,7 +25,13 @@ SECRET_KEY = 'django-insecure-19de2gixdvqas)o&1h7v-4l+bbe9k+3d(4rh8tr)5l1fb_ic=v
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '*',
+    'localhost',
+    '127.0.0.1',
+    'http://localhost:5173',
+    '127.0.0.1:5173',
+        ]
 
 
 # Application definition
@@ -37,11 +43,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -121,3 +129,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+ # for development only,testing integration between react and django
+CORS_ALLOWED_ORIGINS = [ 'http://localhost:5173',
+                         'http://127.0.0.1:5173', ]
